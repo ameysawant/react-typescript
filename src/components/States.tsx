6f8770032f8c173d5063aca0329543a8a8a4f49b
Amey <mailforamey@gmail.com>
2026-09-25 19:12:14 +0530

    updated

--- src/components/States.tsx
+++ src/components/States.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import Button from "./Button";
 import type { ChangeEvent } from "react";
+import Button from "./Button";
 
 type MyName = string;
-type MyAge = number;
-type Visibility = boolean;
 type InputEvent = ChangeEvent<HTMLInputElement>;
+type MyAge = number;
+type Visibilty = boolean;
 type Hobbies = string[];
 type HobbyInput = string;
 type User = {
@@ -15,8 +15,8 @@ type User = {
 
 const States = () => {
   const [myName, setMyName] = useState<MyName>("");
-  const [myAge, setMyAge] = useState<MyAge>(0);
-  const [visible, setVisible] = useState<Visibility>(false);
+  const [myAge, setMyAge] = useState<MyAge>(45);
+  const [visible, setVisible] = useState<Visibilty>(false);
   const [hobbies, setHobbies] = useState<Hobbies>(["cricket"]);
   const [hobbyInput, setHobbyInput] = useState<HobbyInput>("");
   const [user, setUser] = useState<User>({
@@ -36,7 +36,7 @@ const States = () => {
     setVisible((prev) => !prev);
   };
 
-  const handleMyHobby = (e: InputEvent) => {
+  const handleHobby = (e: InputEvent) => {
     setHobbyInput(e.target.value);
   };
 
@@ -46,7 +46,7 @@ const States = () => {
   };
 
   const handleUserName = (e: InputEvent) => {
-    setUser((prev) => ({ ...prev, name: e.target.value }));
+    setUser((prev) => ({ ...prev, name: String(e.target.value) }));
   };
   const handleUserAge = (e: InputEvent) => {
     setUser((prev) => ({ ...prev, age: Number(e.target.value) }));
@@ -54,34 +54,35 @@ const States = () => {
 
   return (
     <>
-      <h2>States</h2>
-      <p>Name Example</p>
+      <h2>states</h2>
+
+      <p>string example</p>
       <input
         type="text"
-        placeholder="name"
+        placeholder="myname"
         value={myName}
         onChange={handleMyName}
       />
       {myName}
 
-      <p>Age Example</p>
+      <p>age example</p>
       <Button variant="primary" size="sm" onClick={handleMyAge}>
         increase my age
       </Button>
       {myAge}
 
-      <p>Visibility</p>
+      <p>visible example</p>
       <Button variant="primary" size="sm" onClick={handleToggle}>
         {visible ? "Hide" : "Show"}
       </Button>
-      {visible ? "i am visible" : "not visible"}
+      {visible ? "visible" : "not visible"}
 
-      <p>Hobbies</p>
+      <p>hobbies example</p>
       <input
         type="text"
-        placeholder="hobbies"
+        placeholder="hobby"
         value={hobbyInput}
-        onChange={handleMyHobby}
+        onChange={handleHobby}
       />
       <Button variant="primary" size="sm" onClick={addHobby}>
         add hobby
@@ -90,16 +91,16 @@ const States = () => {
         return <p key={index}>{item}</p>;
       })}
 
-      <p>User example</p>
+      <p>object example</p>
       <input
         type="text"
-        placeholder="name"
+        placeholder="username"
         value={user.name}
         onChange={handleUserName}
       />
       <input
         type="text"
-        placeholder="age"
+        placeholder="userage"
         value={user.age}
         onChange={handleUserAge}
       />
